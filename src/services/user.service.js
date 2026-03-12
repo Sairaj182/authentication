6af085f9561b7474522d32187ea12889173fc0f6
Sairaj182 <sairajraithatha.official@gmail.com>
2026-03-12 17:02:27 +0530

    Add Docker support with Dockerfile and docker-compose.yml; update user service to exclude sensitive attributes for security reasons

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,13 +4,17 @@ const AppError = require("../errors/AppError");
 class UserService {
 
 	async getProfile(userId) {
-		const user = await userRepo.findById(userId);
+		const user = await userRepo.findByPk(id, {
+			attributes: { exclude: ["password", "refreshToken", "tokenVersion"] }
+		});
 		if (!user) throw new AppError("User not found",404);
 		return user;
 	}
 
 	async updateProfile(userId,data) {
-		const user = await userRepo.findById(userId);
+		const user = await userRepo.findByPk(id, {
+			attributes: { exclude: ["password", "refreshToken", "tokenVersion"] }
+		});
 		if (!user) throw new AppError("User not found",404);
 
 		await user.update(data);
@@ -18,13 +22,17 @@ class UserService {
 	}
 
 	async getUserById(id){
-		const user = await userRepo.findById(id);
+		const user = await userRepo.findByPk(id, {
+			attributes: { exclude: ["password", "refreshToken", "tokenVersion"] }
+		});
 		if(!user) throw new AppError("User not found",404);
 		return user;
 	}
 
 	async updateUserByAdmin(id,data){
-		const user = await userRepo.findById(id);
+		const user = await userRepo.findByPk(id, {
+			attributes: { exclude: ["password", "refreshToken", "tokenVersion"] }
+		});
 		if(!user) throw new AppError("User not found",404);
 
 		await user.update(data);
